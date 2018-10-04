935604b143a74c0e8dc77e32ddce5375b598d906
Justus Bonz <justus.bonz@gmail.com>
2018-10-04 17:08:33 +0200

    Added an answer helper

---
+++ KlausurtrainerPlugin/KlausurtrainerPlugin/Helper/AnswerHelper.cs
@@ -0,0 +1,27 @@
+﻿using KlausurtrainerPlugin.Enum;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KlausurtrainerPlugin.Helper
+{
+    public static class AnswerHelper
+    {
+        public static AnswerStatus GetStatus(this Dictionary<Cell, AnswerCellStatus> answerStatus)
+        {
+            var values = answerStatus.Values;
+            if (values.Any(x => x == AnswerCellStatus.NotValidated))
+            {
+                return AnswerStatus.NotValidated;
+            }
+            else if (values.Any(x => x == AnswerCellStatus.SyntaxError || x == AnswerCellStatus.Missing))
+            {
+                return AnswerStatus.SyntaxError;
+            }
+            else if(values.Any(x => x == AnswerCellStatus.Incorrect))
+            {
+                return AnswerStatus.Incorrect;
+            }
+            return AnswerStatus.Correct;
+        }
+    }
+}
